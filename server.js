118469a0306323497d1efc4f3461df29335f449d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-09 17:33:00 -0300

    cria front end

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//   },
+// });
+
+app.use(cors());
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+app.set('views', './views');
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (_req, res) => {
+  res.render('index.html');
+});
+
+const PORT = 3000;
+http.listen(PORT, () => { console.log(`Ouvindo na porta ${PORT}`); });
